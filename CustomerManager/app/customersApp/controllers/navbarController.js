9c70090a42f72a76aabbd1fbdb081f1a4bd5e45f
Girish <girish@post.com>
2014-07-23 20:06:44 -0700

    No js error on index.html page

---
+++ CustomerManager/app/customersApp/controllers/navbarController.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    var NavbarController = function($scope) {
+        var appTitle = 'Customer Management';
+        $scope.appTitle = appTitle;
+    };
+
+    NavbarController.$inject = ['$scope'];
+
+    angular.module('customersApp').controller('NavbarController', NavbarController);
+
+}());
