72269f624cacaa7e7bcadaa7db459b7f6b01d235
Girish <girish@post.com>
2014-07-23 20:32:33 -0700

    added init

--- CustomerManager/app/customersApp/controllers/customers/customersController.js
+++ CustomerManager/app/customersApp/controllers/customers/customersController.js
@@ -1,6 +1,17 @@
 ﻿(function() {
-    var CustomersController = function($scope) {
+    var CustomersController = function ($scope) {
 
+
+       
+
+        function init() {
+            getCustomersSummary();
+        }
+        var getCustomersSummary = function() {
+            
+        };
+
+        init();
     };
 
     CustomersController.$inject = ['$scope'];
